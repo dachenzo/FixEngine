2a226a12793a6a5f84fb6a9f5fbe9f080ca09ea0
Michael Okeke <michaelokeke101@gmail.com>
2026-01-08 14:41:36 +0000

    Added message generation for evry admin message

--- include/fix/core/MessageFactory.hpp
+++ include/fix/core/MessageFactory.hpp
@@ -76,6 +76,7 @@ namespace Fix {
 
 
         std::string_view logon(int heartbeat_override, bool echo_reset) {
+            scratch_.reset();
             stamp_header_("A");
             scratch_.add_field(98, params_.encrypt_method_str);
             scratch_.add_field(141, (echo_reset ? "Y" : "N"));
@@ -83,11 +84,53 @@ namespace Fix {
             stamp_trailer_();
             return scratch_.get_buffer_view();
         }
-        Fix::ValidMessage heart_beat(std::optional<std::string> test_req_id = std::nullopt);
-        Fix::ValidMessage test_request(std::string id);
-        Fix::ValidMessage resend_request(int begin_seq_no, int end_seq_no);
-        Fix::ValidMessage sequence_reset(int newSeqNo, bool gapfill);
-        Fix::ValidMessage logout(std::string text = {});
+
+        std::string_view heart_beat(std::optional<std::string> test_req_id = std::nullopt) {
+            scratch_.reset();
+            stamp_header_("0");
+            if (test_req_id.has_value()) {
+                scratch_.add_field(112, test_req_id.value());
+            }
+            stamp_trailer_();
+            return scratch_.get_buffer_view();
+        }
+        std::string_view test_request(std::string id) {
+            scratch_.reset();
+            stamp_header_("1");
+            scratch_.add_field(112, id);
+            stamp_trailer_();
+            return scratch_.get_buffer_view();
+        }
+
+        std::string_view resend_request(int begin_seq_no, int end_seq_no) {
+            scratch_.reset();
+            stamp_header_("2");
+            scratch_.add_field(7,  std::to_string(begin_seq_no));
+            scratch_.add_field(16, std::to_string(end_seq_no));
+            stamp_trailer_();
+            return scratch_.get_buffer_view();
+        }
+        std::string_view sequence_reset(int newSeqNo, bool gapfill) {
+            scratch_.reset();
+            stamp_header_("4");
+            if (gapfill) {
+                scratch_.add_field(123, "Y");   // GapFillFlag
+            }
+            scratch_.add_field(36, std::to_string(newSeqNo)); // NewSeqNo
+            // If this is going out in a resend stream: add 43=Y and 122=...
+            stamp_trailer_();
+            return scratch_.get_buffer_view();
+        }
+        
+        std::string_view logout(std::string text = {}) {
+            scratch_.reset();
+            stamp_header_("5");
+            if (!text.empty()) {
+                scratch_.add_field(58, text);
+            }
+            stamp_trailer_();
+            return scratch_.get_buffer_view();
+        }
 
 
 
@@ -98,7 +141,6 @@ namespace Fix {
         TClock& clock_;
 
         void stamp_header_(std::string type) {
-            scratch_.reset();
             scratch_.add_field(8, params_.fix_version);
             scratch_.add_body_length_placeholder();
             scratch_.add_field(35, type);
