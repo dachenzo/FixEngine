b4064b6e56c4838cde437970beabde6899e96b11
Michael Okeke <michaelokeke101@gmail.com>
2026-02-06 11:00:57 +0000

    added a basic example

--- include/fix/log/LogContext.hpp
+++ include/fix/log/LogContext.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <string>
-
 #include <fix/error/Category.hpp>
 #include <fix/error/Layer.hpp>
 #include <fix/error/Severity.hpp>
--- include/fix/log/LogFile.hpp
+++ include/fix/log/LogFile.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #include <string>
 #include <fstream>
-#include <filesystem>
 #include <chrono>
 
 namespace Fix::Log {
